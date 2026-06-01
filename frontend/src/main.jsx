8cd3ca49b8bba7cb25ff5f20e446c0a5e90225c6
adityasomde <adityasomde9@gmail.com>
2026-06-01 08:14:00 +0530

    Release desktop app version 0.1.2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-const isDesktop =
-  typeof window !== "undefined" &&
-  !!window.printeaseDesktop &&
-  !!window.printeaseDesktop.isDesktop;
-
-const Router = isDesktop ? HashRouter : BrowserRouter;
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );

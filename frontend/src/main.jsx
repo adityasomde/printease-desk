fc07332cf8dfc141fd2f451d6e4342d49b0d09e3
adityasomde <adityasomde9@gmail.com>
2026-06-01 08:05:40 +0530

    desktop: use HashRouter in Electron to support reloads

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+const isDesktop =
+  typeof window !== "undefined" &&
+  !!window.printeaseDesktop &&
+  !!window.printeaseDesktop.isDesktop;
+
+const Router = isDesktop ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
